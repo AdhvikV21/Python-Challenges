33ff6089e9ee37d6e80a21951b97f6a2e172d32d
AdhvikV21 <adzydarock@gmail.com>
2025-08-20 19:13:36 +1000

    Add files via upload

---
+++ Number swap.py
@@ -0,0 +1,13 @@
+#Take input from the user
+x = input("Please enter a number")
+y = input("Please enter a number")
+
+#swapping
+a = x
+x = y
+y = a
+
+#Displaying results after swapping
+print("The value of x and y after swapping", x)
+print("The value of y and x after swapping", y)
+
